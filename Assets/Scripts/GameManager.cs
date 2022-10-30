b1aa97fd7ca10ec4e5dca2895aa08f51f5c7c8cc
tularemo22 <antony1994@hotmail.es>
2022-10-29 22:21:49 -0300

    Pantalla de inicio creada

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -16,12 +17,13 @@ public class GameManager : MonoBehaviour
         {
             Instance = this;
         }
+        
     }
 
     [SerializeField] GameObject mordiendo;
     [SerializeField] GameObject vampiro;
 
-   public IEnumerator PantallaDeMordida()
+    public IEnumerator PantallaDeMordida()
     {   //Activar vampiro de fondo
         Debug.Log("Iniciando coorrutina");
         vampiro.gameObject.SetActive(true);
@@ -37,5 +39,22 @@ public class GameManager : MonoBehaviour
         mordiendo.SetActive(false);
     }
 
-   
+    //Funcion para cambio de escenas
+
+    public void CambiarEscena()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void RecargarEscena()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    
+    public void SalirDelJuego()
+    {
+        Application.Quit();
+        Debug.Log("No se puede aplicar en unity pero si aparece este cartel es porque cerramos la aplicacion con exito");
+    }
+
 }
